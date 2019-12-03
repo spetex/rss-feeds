fdf23f51dc8ae3b3b8b9cf86eee5135b2b470831
Petr Sahula <spetex@icloud.com>
2019-12-03 21:00:50 +0100

    Rodify my Sinatra

--- server.rb
+++ server.rb
@@ -1,32 +1,38 @@
-require 'sinatra'
+require 'roda'
 require 'rss'
 require 'json'
 
 DATA_DIR = ENV['CRAWLERS_DATA_DIR']
 
-def produceRSS(events)
-  rss = RSS::Maker.make("atom") do |maker|
-    maker.channel.author = "GoOut"
+def produce_rss(events)
+  rss = RSS::Maker.make('atom') do |maker|
+    maker.channel.author = 'GoOut'
     maker.channel.updated = Time.now.to_s
-    maker.channel.about = "List of just announced events in Prague"
-    maker.channel.title = "GoOut.cz Praha Newly Announced"
+    maker.channel.about = 'List of just announced events in Prague'
+    maker.channel.title = 'GoOut.cz Praha Newly Announced'
 
     events.each do |event|
       maker.items.new_item do |item|
-        item.link = event["link"]
-        item.title = event["name"]
-        item.updated = event["scrapeDate"]
-        item.description = "#{event["dateTime"]} - #{event["venue"]}"
+        item.link = event['link']
+        item.title = event['name']
+        item.updated = event['scrapeDate']
+        item.description = "#{event['dateTime']} - #{event['venue']}"
       end
     end
   end
 end
 
-class RssProviderApp < Sinatra::Base
-  get '/goout.rss', :provides => ['rss', 'atom', 'xml'] do
-    file = File.open "#{DATA_DIR}/goout_newly_announced.json"
-    events = JSON.load file
-    rss = produceRSS events
-    rss.to_s
+class RssProviderApp < Roda
+  route do |r|
+    r.on 'goout.rss' do
+      response['Content-Type'] = 'application/xml'
+      file = File.open "#{DATA_DIR}/goout_newly_announced.json"
+      events = JSON.load file
+      rss = produce_rss events
+      rss.to_s
+    end
+    r.on do
+      '404: Not found'
+    end
   end
 end
